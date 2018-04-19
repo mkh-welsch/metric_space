8ea681a5bd090023565212195d69ac322a0bec28
Michael Welsch <michael.welsch@hsu-hh.de>
2018-04-19 19:10:44 +0200

    Update metric_search.cpp

--- metric_search.cpp
+++ metric_search.cpp
@@ -1,4 +1,11 @@
-﻿#include "metric_search.hpp" // back reference for header only use
+/* Michael Welsch (c) 2018
+
+This Source Code Form is subject to the terms of the Mozilla Public
+License, v. 2.0. If a copy of the MPL was not distributed with this
+file, You can obtain one at http://mozilla.org/MPL/2.0/.*/
+
+
+#include "metric_search.hpp" // back reference for header only use
 
 namespace metric_search
 {
