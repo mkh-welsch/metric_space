01aa385920feb8ab82cd4c3e64786f19caa268eb
Michael Welsch <welsch@panda.technology>
2019-02-22 11:49:24 +0100

    added graph class entry files.

---
+++ details/graph.cpp
@@ -0,0 +1,2 @@
+#include "graph.hpp"
+#include "graph/connected_components.hpp"
---
+++ details/graph.hpp
@@ -0,0 +1,14 @@
+/* 
+This Source Code Form is subject to the terms of the Mozilla Public
+License, v. 2.0. If a copy of the MPL was not distributed with this
+file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+Signal Empowering Technology ®Michael Welsch
+*/
+
+#ifndef _METRIC_GRAPH_HPP
+#define _METRIC_GRAPH_HPP
+
+#include "graph.cpp"
+
+#endif // header guards
